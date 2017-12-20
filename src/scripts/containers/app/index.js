fbfcde4b5e0159a830bb4ad40d08969bdea6f064
typhoonIscoming <850525344@qq.com>
2017-12-20 21:47:21 +0800

    init

--- src/scripts/containers/app/index.js
+++ src/scripts/containers/app/index.js
@@ -1,18 +1,15 @@
 import App from "../../uicomponents/app";
 
 import {connect} from "react-redux";
-import {changeswipe} from "../../actions";
+
 const mapStateToProps = (state)=>{
     return {
-        swipeindex:state.swipeindex
     }
 }
 const mapDispatchToProps = (dispatch)=>{
     return{
         dispatch,
-        changeswipe:(index)=>{
-            dispatch(changeswipe(index));
-        }
+        
     }
 }
 
